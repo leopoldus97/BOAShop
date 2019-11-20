c403fbcb537f2b14da44dce572987cc8a20a2de8
Máté <mate1011@easv365.dk>
2019-11-20 13:23:25 +0100

    + navbar, footer, welcome-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
