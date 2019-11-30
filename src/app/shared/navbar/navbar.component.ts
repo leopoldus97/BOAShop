67a81d6feac6c9ca70c935b9de9555be94c88a0b
Máté <mate1011@easv365.dk>
2019-11-30 15:46:34 +0100

    +Shopping cart, navbar redesigned

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  cartItems: number;
   constructor() { }
 
   ngOnInit() {
+    this.cartItems = 2;
   }
 
 }
