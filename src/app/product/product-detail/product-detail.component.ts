83849475d2b4c6e21905e7608f219bee362c5ced
Szymon Soczewka <sz.soczewka@gmail.com>
2019-12-02 19:46:04 +0100

    .

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -22,7 +22,5 @@ export class ProductDetailComponent implements OnInit {
     this.service.getProductByID(id).subscribe(product => this.product = product);
   }
 
-  activate(){
-    $().button('toggle');
-  }
+
 }
