3e8f7740f7cbb42d3d81a068ec46ae3a239d80e0
Szymon Soczewka <sz.soczewka@gmail.com>
2019-12-04 22:25:48 +0100

    Collections in dropdown menu are loaded from database from now on

--- src/app/collection/collection-list/collection-list.component.ts
+++ src/app/collection/collection-list/collection-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Collection} from '../../shared/models/collection';
 import {CollectionService} from '../../shared/services/collection.service';
 import {ProductService} from '../../shared/services/product.service';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-collection-list',
@@ -10,7 +11,7 @@ import {ProductService} from '../../shared/services/product.service';
 })
 export class CollectionListComponent implements OnInit {
   collections: Collection[];
-  constructor(private collectionService: CollectionService, private productService: ProductService) { }
+  constructor(private router: Router, private collectionService: CollectionService, private productService: ProductService) { }
 
   ngOnInit() {
     this.getCollections();
@@ -19,5 +20,9 @@ export class CollectionListComponent implements OnInit {
   private getCollections() {
     this.collectionService.getCollections().subscribe(
       collections => {this.collections = collections; });
+  }
+  setFilter(specification: string, routeTail: string, properType: string) {
+    this.productService.setFilter(specification, properType);
+    this.router.navigateByUrl(  'products' + routeTail);
   }
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import {Component, NgZone, OnInit} from '@angular/core';
 import {ProductService} from '../services/product.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {CollectionService} from "../services/collection.service";
+import {Collection} from "../models/collection";
 
 @Component({
   selector: 'app-navbar',
@@ -10,13 +12,19 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   cartItems: number;
-  constructor(private productService: ProductService, private router: Router) {}
+  collections: Collection[];
+  constructor(private productService: ProductService, private router: Router, private collectionService: CollectionService) {}
 
     ngOnInit() {
+    this.getCollections();
     this.cartItems = 2;
   }
   setFilter(specification: string, routeTail: string, properType: string) {
     this.productService.setFilter(specification, properType);
     this.router.navigateByUrl('products' + routeTail);
+  }
+  private getCollections() {
+    this.collectionService.getCollections().subscribe(
+      collections => {this.collections = collections; });
   }
 }
