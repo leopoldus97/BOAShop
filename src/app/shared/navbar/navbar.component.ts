bb36168b966885985dcbb761bdb73cecea5b48d8
Máté <mate1011@easv365.dk>
2019-12-05 12:45:59 +0100

    +Search function

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
 import {Component, NgZone, OnInit} from '@angular/core';
 import {ProductService} from '../services/product.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {CollectionService} from "../services/collection.service";
-import {Collection} from "../models/collection";
+import {CollectionService} from '../services/collection.service';
+import {Collection} from '../models/collection';
 
 @Component({
   selector: 'app-navbar',
@@ -12,12 +12,24 @@ import {Collection} from "../models/collection";
 })
 export class NavbarComponent implements OnInit {
   cartItems: number;
+  searchItem: string;
+  itemList: [string, string, string, string, string, string, string];
   collections: Collection[];
   constructor(private productService: ProductService, private router: Router, private collectionService: CollectionService) {}
 
     ngOnInit() {
     this.getCollections();
     this.cartItems = 2;
+    this.searchItem = '';
+    this.itemList = [
+      'T-Shirts',
+      'Hoodies And Sweatshirts',
+      'Accessories',
+      'Tops',
+      'Dresses And Skirts',
+      'Sportswear',
+      'Swimwear'
+    ];
   }
   setFilter(specification: string, routeTail: string, properType: string) {
     this.productService.setFilter(specification, properType);
@@ -26,5 +38,17 @@ export class NavbarComponent implements OnInit {
   private getCollections() {
     this.collectionService.getCollections().subscribe(
       collections => {this.collections = collections; });
+  }
+  search() {
+    if (this.searchItem.trim() !== '') {
+      const itemType = this.itemList.find( e => e.toLowerCase().includes(this.searchItem.toLowerCase()));
+      const spec = 'type=' + itemType;
+      const routeTail = itemType.replace(' ', '-');
+      this.productService.setFilter(spec, itemType);
+      this.router.navigateByUrl('/products/' + routeTail);
+    } else {
+      this.productService.setFilter('', 'Available Products');
+      this.router.navigateByUrl('/products');
+    }
   }
 }
