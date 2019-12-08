a84de6fe9d80d54f6a14907c3131b0107fb3a064
Szymon Soczewka <sz.soczewka@gmail.com>
2019-12-08 15:11:53 +0100

    Change tracker conflict resolved

--- src/app/admin/admin-collection/admin-collection.component.ts
+++ src/app/admin/admin-collection/admin-collection.component.ts
@@ -14,19 +14,7 @@ import {forEachComment} from 'tslint';
 export class AdminCollectionComponent implements OnInit {
 
   collection: Collection  = {id: 0, name: '', products: []};
-  collProduct: Product = {
-    id: 0,
-    availableQuantity: 0,
-    collection: null,
-    description: '',
-    discountPrice: 0,
-    gender: '',
-    name: '',
-    pictures: [],
-    price: 0,
-    sizeQuantity: null,
-    type: ''
-  };
+  collProduct: Product;
   allProducts: Product[];
   displayProducts = false;
   alreadyAdded: number[] = [];
@@ -37,24 +25,48 @@ export class AdminCollectionComponent implements OnInit {
     ) { }
 
   ngOnInit() {
+    this.initProduct()
     this.proSer.getProducts().subscribe(data => this.allProducts = data);
   }
 
   showProducts() {
+    this.collProduct.id = this.allProducts[0].id;
     this.displayProducts = true;
+    document.getElementById('addButton').hidden = true;
   }
   transfer() {
-    const prod = this.alreadyAdded.some( productID => productID === this.collProduct.id );
-    if (prod === false) {
-      this.proSer.getProductByID(this.collProduct.id).subscribe(data => {
-        this.collection.products.push(data);
-        this.alreadyAdded.push(this.collProduct.id);
-      });
+    const prod = this.alreadyAdded.some(productID => productID === this.collProduct.id);
+    if (prod === false && this.collProduct.id !== 0) {
+      this.collection.products.push(this.collProduct);
+      this.alreadyAdded.push(this.collProduct.id);
+      this.initProduct();
     }
   }
   save() {
     this.colSer.createCollection(this.collection).subscribe(() => this.router.navigateByUrl('/admin'));
   }
 
-
+  setID() {
+    const e = (document.getElementById('select')) as HTMLSelectElement;
+    const sel = e.selectedIndex;
+    const opt = e.options[sel];
+    const CurValue = (opt as unknown as HTMLSelectElement).value;
+    const valueAsNumber = Number(CurValue);
+    this.collProduct.id = valueAsNumber;
+  }
+  initProduct() {
+    this.collProduct = {
+      id: 0,
+      availableQuantity: 0,
+      collection: null,
+      description: '',
+      discountPrice: 0,
+      gender: '',
+      name: '',
+      pictures: [],
+      price: 0,
+      sizeQuantity: null,
+      type: ''
+    };
+  }
 }
