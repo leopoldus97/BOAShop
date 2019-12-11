fc545c559254290e90b7210d7f939d6467fec92a
Mads <mads_beier@hotmail.com>
2019-12-11 14:00:29 +0100

    Admin product list ordering added

--- src/app/admin/admin-list/admin-list.component.ts
+++ src/app/admin/admin-list/admin-list.component.ts
@@ -10,16 +10,80 @@ import {Product} from '../../shared/models/product';
 export class AdminListComponent implements OnInit {
 
   allProducts: Product[];
+  idSort = '';
+  nameSort = '';
+  typeSort = '';
+  priceSort = '';
 
   constructor(private service: ProductService) { }
 
   ngOnInit() {
-    this.service.getProducts().subscribe(listOfProducts => this.allProducts = listOfProducts);
+    this.getProducts();
     document.getElementById('btn-all-products').setAttribute('style',
       'background-color:#000000; color: white; width:200px');
     document.getElementById('btn-add-product').setAttribute('style', '');
     document.getElementById('btn-new-collection').setAttribute('style', '');
     document.getElementById('btn-collection-list').setAttribute('style', '');
+  }
+
+  sortID() {
+    if (this.idSort === '' || this.idSort === '▼') {
+      this.service.setFilter('order=IDAsc', '');
+      this.idSort = '▲';
+    } else if (this.idSort === '▲') {
+      this.service.setFilter('order=IDDsc', '');
+      this.idSort = '▼';
+    }
+    this.nameSort = '';
+    this.priceSort = '';
+    this.typeSort = '';
+    this.getProducts();
+  }
+
+  sortName() {
+    if (this.nameSort === '' || this.nameSort === '▼') {
+      this.service.setFilter('order=nameAsc', '');
+      this.nameSort = '▲';
+    } else if (this.nameSort === '▲') {
+      this.service.setFilter('order=nameDsc', '');
+      this.nameSort = '▼';
+    }
+    this.idSort = '';
+    this.priceSort = '';
+    this.typeSort = '';
+    this.getProducts();
+  }
+
+  sortType() {
+    if (this.typeSort === '' || this.typeSort === '▼') {
+      this.service.setFilter('order=typeAsc', '');
+      this.typeSort = '▲';
+    } else if (this.typeSort === '▲') {
+      this.service.setFilter('order=typeDsc', '');
+      this.typeSort = '▼';
+    }
+    this.idSort = '';
+    this.priceSort = '';
+    this.nameSort = '';
+    this.getProducts();
+  }
+
+  sortPrice() {
+    if (this.priceSort === '' || this.priceSort === '▼') {
+      this.service.setFilter('order=priceAsc', '');
+      this.priceSort = '▲';
+    } else if (this.priceSort === '▲') {
+      this.service.setFilter('order=priceDsc', '');
+      this.priceSort = '▼';
+    }
+    this.idSort = '';
+    this.nameSort = '';
+    this.typeSort = '';
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.service.getProducts().subscribe(listOfProducts => this.allProducts = listOfProducts);
   }
 
 }
