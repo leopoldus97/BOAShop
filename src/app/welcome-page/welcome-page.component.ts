b75cbe3aa7da5df4ee2407d9ff76fc4664991dc0
Szymon Soczewka <sz.soczewka@gmail.com>
2019-12-11 14:36:51 +0100

    Slideshow

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../shared/services/product-service/product.service';
+import {listenerCount} from "cluster";
+import {Product} from '../shared/models/product';
+import {CollectionService} from "../shared/services/collection-service/collection.service";
+import {Collection} from "../shared/models/collection";
 
 @Component({
   selector: 'app-welcome-page',
@@ -6,10 +11,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent implements OnInit {
+  private collection: Collection;
 
-  constructor() { }
-
+  constructor(private collectionService: CollectionService) {}
   ngOnInit() {
+    this.getProducts();
   }
-
+  getProducts() {
+    this.collectionService.getCollections().subscribe( col => this.collection = col[0] );
+  }
 }
